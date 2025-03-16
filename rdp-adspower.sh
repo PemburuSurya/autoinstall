149f71721e78f219353c9290065c531ef2362713
Gita Intan <pemburusurya@gmail.com>
2025-03-17 00:02:03 +0800

    Update rdp-adspower.sh

--- rdp-adspower.sh
+++ rdp-adspower.sh
@@ -9,9 +9,49 @@ GREEN='\033[0;32m'  # Green
 BLUE='\033[0;34m'   # Blue
 NC='\033[0m'        # No Color
 
-# Define username and password
-USER="egan"
-PASSWORD="rumiyah123"
+# Display social details and channel information in large letters manually
+echo "========================================"
+echo -e "${YELLOW} Script is made by CRYTONODEHINDI${NC}"
+echo -e "-------------------------------------"
+
+# Large ASCII Text with BANNER color
+echo -e "${BANNER}  CCCCC  RRRRR   Y   Y  PPPP   TTTTT  OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III${NC}"
+echo -e "${BANNER} C       R   R    Y Y   P  P     T   O   O     NN  N  O   O  D   D E          H   H   I   NN  N  D   D   I ${NC}"
+echo -e "${BANNER} C       RRRRR     Y    PPPP     T   O   O     N N N  O   O  D   D EEEE       HHHHH   I   N N N  D   D   I ${NC}"
+echo -e "${BANNER} C       R   R     Y     P       T   O   O     N  NN  O   O  D   D E          H   H   I   N  NN  D   D   I ${NC}"
+echo -e "${BANNER}  CCCCC  R    R    Y     P       T    OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III${NC}"
+
+echo "============================================"
+
+# Use different colors for each link to make them pop out more
+echo -e "${YELLOW}Telegram: ${GREEN}https://t.me/cryptonodehindi${NC}"
+echo -e "${YELLOW}Twitter: ${GREEN}@CryptonodeHindi${NC}"
+echo -e "${YELLOW}YouTube: ${GREEN}https://www.youtube.com/@CryptonodesHindi${NC}"
+echo -e "${YELLOW}Medium: ${BLUE}https://medium.com/@cryptonodehindi${NC}"
+
+echo "============================================="
+
+# Prompt for username and password
+while true; do
+    read -p "Enter the username for remote desktop: " USER
+    if [[ "$USER" == "root" ]]; then
+        echo -e "${RED}Error: 'root' cannot be used as the username. Please choose a different username.${NC}"
+    elif [[ "$USER" =~ [^a-zA-Z0-9] ]]; then
+        echo -e "${RED}Error: Username contains forbidden characters. Only alphanumeric characters are allowed.${NC}"
+    else
+        break
+    fi
+done
+
+while true; do
+    read -sp "Enter the password for $USER: " PASSWORD
+    echo
+    if [[ "$PASSWORD" =~ [^a-zA-Z0-9] ]]; then
+        echo -e "${RED}Error: Password contains forbidden characters. Only alphanumeric characters are allowed.${NC}"
+    else
+        break
+    fi
+done
 
 # Update and install required packages
 echo -e "${INFO}Updating package list...${NC}"
@@ -98,3 +138,25 @@ echo -e "${INFO}You can now connect via Remote Desktop with the following detail
 echo -e "${INFO}IP ADDRESS: ${GREEN}$IP_ADDR${NC}"
 echo -e "${INFO}USER: ${GREEN}$USER${NC}"
 echo -e "${INFO}PASSWORD: ${GREEN}$PASSWORD${NC}"
+
+# Display thank you message
+echo "========================================"
+echo -e "${YELLOW} Thanks for using the script${NC}"
+echo -e "-------------------------------------"
+
+# Large ASCII Text with BANNER color
+echo -e "${BANNER}  CCCCC  RRRRR   Y   Y  PPPP   TTTTT  OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III${NC}"
+echo -e "${BANNER} C       R   R    Y Y   P  P     T   O   O     NN  N  O   O  D   D E          H   H   I   NN  N  D   D   I ${NC}"
+echo -e "${BANNER} C       RRRRR     Y    PPPP     T   O   O     N N N  O   O  D   D EEEE       HHHHH   I   N N N  D   D   I ${NC}"
+echo -e "${BANNER} C       R   R     Y    P        T   O   O     N  NN  O   O  D   D E          H   H   I   N  NN  D   D   I ${NC}"
+echo -e "${BANNER}  CCCCC  R    R    Y    P        T    OOO      N   N   OOO   DDDD  EEEEE      H   H  III  N   N  DDDD   III${NC}"
+
+echo "============================================"
+
+# Use different colors for each link to make them pop out more
+echo -e "${YELLOW}Telegram: ${GREEN}https://t.me/cryptonodehindi${NC}"
+echo -e "${YELLOW}Twitter: ${GREEN}@CryptonodeHindi${NC}"
+echo -e "${YELLOW}YouTube: ${GREEN}https://www.youtube.com/@CryptonodesHindi${NC}"
+echo -e "${YELLOW}Medium: ${CYAN}https://medium.com/@cryptonodehindi${NC}"
+
+echo "============================================="
