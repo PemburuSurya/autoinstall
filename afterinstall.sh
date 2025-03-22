d0c443ed1bdefd7b89753ed9aa83169bb8e1ac02
Gita Intan <pemburusurya@gmail.com>
2025-03-23 06:30:45 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -94,16 +94,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
-# Hapus file resolv.conf
-sudo rm /etc/resolv.conf
-
-# Buat file resolv.conf baru dan tambahkan nameserver
-sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
-sudo bash -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
-
-# Lock file resolv.conf agar tidak bisa diubah
-sudo chattr +i /etc/resolv.conf
-
 # Unduh Anaconda installer
 cd /tmp
 echo "Mengunduh Anaconda installer..."
@@ -175,5 +165,4 @@ echo "Instalasi selesai!"
 echo "- Docker dan Docker Compose telah diinstal."
 echo "- Anaconda telah diinstal/diperbarui dan lingkungan virtual 'myenv' telah dibuat."
 echo "- Rust, Visual Studio Code, dan alat-alat pengembangan lainnya telah diinstal."
-echo "- DNS Telah dirubah ke Google DNS."
 echo "================================================"
