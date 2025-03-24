c019a90e83e8024336f9aa1a0f071c15708168ba
Gita Intan <pemburusurya@gmail.com>
2025-03-24 13:47:59 +0800

    Update dns-swap.sh

--- dns-swap.sh
+++ dns-swap.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Hapus file resolv.conf yang ada
-echo -e "\033[0;32mMenghapus file /etc/resolv.conf...\033[0m"
-sudo rm /etc/resolv.conf
-
-# Buat file resolv.conf baru dan tambahkan nameserver
-echo -e "\033[0;32mMembuat file /etc/resolv.conf baru...\033[0m"
-sudo tee /etc/resolv.conf > /dev/null <<EOL
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOL
-
-# Lock file resolv.conf agar tidak bisa diubah
-echo -e "\033[0;32mMengunci file /etc/resolv.conf...\033[0m"
-sudo chattr +i /etc/resolv.conf
-
 # Nonaktifkan semua swap yang sedang aktif
 echo -e "\033[0;32mMenonaktifkan swap yang sedang aktif...\033[0m"
 sudo swapoff -a
@@ -38,10 +23,12 @@ sudo swapon /swapfile
 # Set vm.overcommit_memory ke 1 secara langsung
 echo -e "\033[0;32mMengatur vm.overcommit_memory ke 1...\033[0m"
 sudo sysctl -w vm.overcommit_memory=1
+sudo sysctl -w vm.swappiness=30
 
 # Set vm.overcommit_memory ke 1 secara permanen
 echo -e "\033[0;32mMengatur vm.overcommit_memory ke 1 secara permanen...\033[0m"
 echo 'vm.overcommit_memory=1' | sudo tee -a /etc/sysctl.conf
+echo 'vm.swappiness=30' | sudo tee -a /etc/sysctl.conf
 
 # Selesai
 echo -e "\033[0;32mSemua proses telah selesai.\033[0m"
