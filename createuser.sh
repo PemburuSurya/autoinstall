16b685ccd75a35fa172f0dc504729d1b94210e17
Gita Intan <pemburusurya@gmail.com>
2025-03-25 09:41:58 +0800

    Create createuser.sh

---
+++ createuser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Menghentikan skrip jika ada perintah yang gagal
+
+# Menambahkan pengguna ubuntu dan hosting
+sudo adduser ubuntu --gecos "" --disabled-password
+sudo adduser hosting --gecos "" --disabled-password
+
+# Mengatur password untuk yuni1 dan yuni2
+echo "ubuntu:egan1337" | sudo chpasswd
+echo "hosting:egan1337" | sudo chpasswd
+
+# Memberikan akses root ke yuni1 dan yuni2
+sudo usermod -aG sudo ubuntu
+sudo usermod -aG sudo hosting
