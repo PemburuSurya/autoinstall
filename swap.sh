42b3796bc599211b8f74a5d0f2a5e44c72e33085
Pemburu Surya <andrianegan13@gmail.com>
2025-03-26 08:53:08 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -6,7 +6,7 @@ sudo swapoff -a
 
 # Buat swapfile baru dengan ukuran 64GB
 echo -e "\033[0;32mMembuat swapfile baru dengan ukuran 64GB...\033[0m"
-sudo fallocate -l 16G /swapfile
+sudo fallocate -l 6G /swapfile
 
 # Format swapfile sebagai area swap
 echo -e "\033[0;32mMemformat swapfile sebagai area swap...\033[0m"
@@ -19,7 +19,7 @@ sudo swapon /swapfile
 # Set vm.overcommit_memory ke 1 dan vm.swappiness ke 10 secara langsung
 echo -e "\033[0;32mMengatur vm.overcommit_memory ke 1 dan vm.swappiness ke 10...\033[0m"
 sudo sysctl -w vm.overcommit_memory=1
-sudo sysctl -w vm.swappiness=10
+sudo sysctl -w vm.swappiness=20
 sudo sysctl -w vm.dirty_ratio=60
 sudo sysctl -w vm.dirty_background_ratio=2
 sudo sysctl -w net.core.somaxconn=65535
@@ -29,7 +29,7 @@ sudo sysctl -w net.ipv4.tcp_max_syn_backlog=65535
 # Set vm.overcommit_memory dan vm.swappiness secara permanen
 echo -e "\033[0;32mMengatur vm.overcommit_memory ke 1 dan vm.swappiness ke 10 secara permanen...\033[0m"
 echo 'vm.overcommit_memory=1' | sudo tee -a /etc/sysctl.conf
-echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+echo 'vm.swappiness=20' | sudo tee -a /etc/sysctl.conf
 echo 'vm.dirty_ratio=60' | sudo tee -a /etc/sysctl.conf
 echo 'vm.dirty_background_ratio=2' | sudo tee -a /etc/sysctl.conf
 echo 'net.core.somaxconn=65535' | sudo tee -a /etc/sysctl.conf
