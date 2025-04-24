de6bfc5f1ba86fbf7d200849400833540c2a21aa
Pemburu Surya <pemburusurya@gmail.com>
2025-04-24 23:17:11 +0700

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -43,7 +43,7 @@ info "Installing essential build tools..."
 install_packages \
     git clang cmake build-essential openssl pkg-config libssl-dev \
     wget htop tmux jq make gcc tar ncdu protobuf-compiler \
-    npm default-jdk aptitude squid apache2-utils file lsof \
+    default-jdk aptitude squid apache2-utils file lsof \
     iptables iptables-persistent openssh-server sed lz4 aria2 pv \
     python3 python3-venv python3-pip python3-dev screen snapd flatpak \
     nano automake autoconf nvme-cli libgbm1 libleveldb-dev bsdmainutils unzip
