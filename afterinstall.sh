a4a9244b782e33a8c21c5e3c18f49686f787f60f
Pemburu Surya <pemburusurya@gmail.com>
2025-04-26 05:13:25 +0700

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -222,7 +222,7 @@ info "Yarn ${YARN_VERSION} installed successfully"
 info "Fixing PS1 error in .bashrc..."
 # Ensure PS1 is only set if not already set
 if ! grep -q 'PS1' ~/.bashrc; then
-    echo 'if [ -z "$PS1" ]; then PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "; fi' >> ~/.bashrc
+    echo 'if [ -z "${PS1+x}" ]; then PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "; fi' >> ~/.bashrc
 fi
 
 # Reload .bashrc to apply changes
